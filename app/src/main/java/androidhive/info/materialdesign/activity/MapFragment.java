137f35c5d5e9a2297a2f2457f9584bb0d171a691
hpatel269 <hpatel269@gmail.com>
2015-07-22 20:35:29 -0500

    first commit

---
+++ app/src/main/java/androidhive/info/materialdesign/activity/MapFragment.java
@@ -0,0 +1,41 @@
+package androidhive.info.materialdesign.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidhive.info.materialdesign.R;
+
+/**
+ * Created by hardik on 7/18/2015.
+ */
+public class MapFragment extends Fragment{
+    public MapFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
